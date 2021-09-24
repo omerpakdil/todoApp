c17bd82379d8ba7394c2a6163f0d6c2019229ef9
omerpakdil <callousity@gmail.com>
2021-09-24 11:42:24 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import './App.css';
 import CompletedToDo from './components/completedTodo';
 import ToDoList from './components/todo';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+
+
 function App() {
 
   const [itemList,setItemList] = useState([]);
   const [completedItemList,setCompletedItemList] = useState([]);
+
+
+  useEffect(() => {
+
+    window.JF.login(
+
+      function success(){
+          window.JF.getForms(function(response){
+              for(var i=0; i<response.length; i++){
+                  document.write( "<li> " + response[i].title);
+              }
+          });
+      },
+
+      function error(){
+          window.alert("Could not authorize user");
+      }
+    ); 
+
+    
+
+    
+
+  },[]);
+
+  useEffect(() => {
+
+  },[itemList]);
+
+  useEffect(() => {
+
+  },[completedItemList]);
+
+
 
   console.log(itemList);
   console.log(completedItemList);
 
   return (
     <div id="container">
-      <h1 className="header1">todo</h1>
+       <h1 className="header1">todo</h1>
       <ToDoList 
       itemList={itemList} 
       setItemList={setItemList}
@@ -25,6 +62,8 @@ function App() {
       completedItemList={completedItemList}
       setCompletedItemList={setCompletedItemList}
       />
+      <button onClick={() => axios.post("https://api.jotform.com/form?apiKey=7981f746e3240a3c10e0c1ea44963223",itemList)
+          .then(res => console.log(res))}></button>
     </div>
   );
 }
